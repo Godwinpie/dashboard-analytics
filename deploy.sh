6b534306fdaabd78997753819d6662a3b868ae0b
Godwinpie <godwinsimon145@gmail.com>
2026-02-06 10:31:47 +0530

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Step 1: Changing directory to /home/ubuntu/dashboard_charts..."
-cd /home/ubuntu/dashboard_charts
+cd /home/godwinsimon145/dashboard-analytics
 
 echo "Step 2: Activating virtual environment..."
 source venv/bin/activate
@@ -16,7 +16,8 @@ python manage.py migrate
 echo "Step 4: Reloading systemd manager configuration..."
 sudo systemctl daemon-reload
 
-echo "Step 5: Restarting notion_dashboard service..."
-sudo systemctl restart notion_dashboard
+echo "Step 5: Restarting gunicorn service..."
+sudo systemctl restart gunicorn
+sudo systemctl restart nginx
 
 echo "Deployment script completed successfully."
